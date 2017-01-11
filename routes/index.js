180f4ebdcaea2162a5de69231fc2782febdd7937
Elin Bäcklund <elin.backlund@svt.se>
2017-01-11 15:11:32 +0100

    Parsing post body into body-object with html separated.

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,31 @@ router.get('/:category', function(req, res) {
     res.render('category', { globalMenu: req.menu, categoryName: req.category });
 });
 
+function parseBodyObject(body) {
+  /* If the recursive function finds an html-block it expects to parse a regular
+     text-block and an html-block. */
+  function recParser(rest, bodyList) {
+    if (!rest || !rest.length) { return bodyList; }
+    var parsedChars = 0;
+    var splitted = rest.split('<html-block>', 2);
+    bodyList.push({
+      content: splitted[0]
+    });
+    if (!rest.includes('<html-block>') || !rest.includes('</html-block>')) { return bodyList; }
+    parsedChars += splitted[0].length;
+    splitted.shift();
+    var htmlContent = splitted.join('').split('</html-block>')[0];
+    bodyList.push({
+      content: htmlContent,
+      isHtml: true
+    });
+    parsedChars += '<html-block></html-block>'.length + htmlContent.length;
+    return recParser(rest.slice(parsedChars), bodyList);
+  }
+  if (!body || !body.includes('<html-block>')) { return [{ content: body, type: 'text' }]; }
+  return recParser(body, []);
+}
+
 router.param('post', function(req, res, next, post) {
 
   contentHandler.getContent()
@@ -44,6 +69,13 @@ router.param('post', function(req, res, next, post) {
         req.menu = contentHandler.getMenu( content, post);
 
         req.post = contentHandler.getPost( content, post );
+        /*
+          bodyList: parsed list of objects found in body, based on the tag <html-block>.
+          object: { content: '', isHtml: true or false }
+        */
+        if (req.post && req.post.fields) {
+          req.post.fields.bodyList = parseBodyObject(req.post.fields.body);
+        }
 
         next();
       }
@@ -56,7 +88,9 @@ router.param('post', function(req, res, next, post) {
 });
 
 router.get('/:category/:post', function(req, res) {
-    res.render('post', { globalMenu: req.menu, activePost: req.post.fields });
+    if (req.post && req.menu) {
+      res.render('post', { globalMenu: req.menu, activePost: req.post.fields });
+    }
 });
 
 module.exports = router;
